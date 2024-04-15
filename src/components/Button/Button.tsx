f29c480f72b422bcc5fef575fc426e10cb549752
Angel Talavera <atalavera86@gmail.com>
2024-04-15 12:24:01 +0200

    I - background color customization

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,49 @@
 import { ButtonHTMLAttributes, FC } from 'react';
 import './Button.css';
 
+const sizes = ['small', 'large'] as const;
+type Size = typeof sizes[number];
+
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> { 
-  basic?: boolean;
+  bgcolor?: string;
+  size?: Size;
 };
 
-const Button: FC<ButtonProps> = ({basic, children, ...props}) => {
-  const btnClassName = basic ? 'basic' : '';
+const Button: FC<ButtonProps> = (data) => {
+  const {bgcolor, size, children} = data;
+  let btnStyles = ``;
+  let className = '';
 
-  return <button className={btnClassName} {...props}>{children}</button>
+  const sheets = document.styleSheets;
+  const lastStyleSheet = sheets[sheets.length - 1];
+
+
+  if (bgcolor) {
+    btnStyles += `background-color: ${bgcolor};`;
+  }
+
+  const isValid = (size: Size) => {
+    if (!sizes.includes(size)) {
+      throw new Error('Invalid type provided to size attribute');
+    }
+
+    return true;
+  };
+  
+  if (size && isValid(size)) {
+    className += ` ${size}`;
+  }
+
+  if (btnStyles) {
+    btnStyles = `.custom {
+      ${btnStyles}
+    }`
+    lastStyleSheet.insertRule(btnStyles, lastStyleSheet.cssRules.length);
+    className += ' custom';
+  }
+
+
+  return <button className={className} {...data}>{children}</button>
 };
 
 export default Button;
--- src/pages/Buttons.tsx
+++ src/pages/Buttons.tsx
@@ -4,15 +4,22 @@ import Button from '../components/Button/Button';
 
 const ButtonsPage = () => {
   const snippet = `
+    <Button bgcolor="blueviolet" size="small">Small</Button>
+    <Button bgcolor="blueviolet" type="submit" onClick={() => alert('Hey! Listen!')}>Basic</Button>
+    <Button bgcolor="blueviolet"size="large">Large</Button>
     <Button>No styles</Button>
-    <Button basic type="submit" onClick={() => alert('Hey! Listen!')}>Basic</Button>
   `;
 
   return <>
     <h1>Buttons</h1>
+    <section>
+      <p></p>
+    </section>
     <section className="items-container">
+      <Button bgcolor="blueviolet" size="small">Small</Button>
+      <Button bgcolor="blueviolet" type="submit" onClick={() => alert('Hey! Listen!')}>Basic</Button>
+      <Button bgcolor="blueviolet"size="large">Large</Button>
       <Button>No styles</Button>
-      <Button basic type="submit" onClick={() => alert('Hey! Listen!')}>Basic</Button>
     </section>
     <section>
       <h3>Anatomy</h3>
